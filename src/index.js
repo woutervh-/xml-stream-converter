afb615e7deeeadc464a899c8289c88553b332c6a
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2017-09-27 16:40:39 +0200

    Allow to JSON or to Object stream

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ function normalizeArrayItems(items) {
     }
 }
 
-function getAttributesNode(context, strict) {
+function getAttributesNodeJSON(context, strict) {
     let result = '"$attributes":{';
     let first = true;
     for (const key of Object.keys(context.attributes)) {
@@ -68,7 +68,178 @@ function getAttributesNode(context, strict) {
     return result;
 }
 
-export default function convert(xmlStream, schema, {strict = false, trimText = true} = {}) {
+export function toObject(xmlStream, schema, objectPath, {strict = false, trimText = true}) {
+    const saxStream = sax.createStream(true, {xmlns: false});
+    const objectStream = new stream.Readable({objectMode: true});
+
+    let depth = 0;
+    let pathDepth = 0;
+
+    const rootSchema = schema;
+    const contextStack = [{
+        name: 'root',
+        value: undefined,
+        schema: rootSchema,
+        attributes: {}
+    }];
+
+    saxStream.on('opentag', (node) => {
+        if (depth === pathDepth && objectPath[depth] === node.name) {
+            pathDepth += 1;
+        }
+        depth += 1;
+
+        const context = contextStack[contextStack.length - 1];
+        switch (context.schema.type) {
+            case 'string':
+            case 'integer':
+            case 'number':
+            case 'boolean':
+                if (strict) {
+                    throw new Error('Did not expect element <' + node.name + '> for schema type ' + context.schema.type);
+                } else {
+                    contextStack.push(context);
+                }
+                break;
+            case 'object': {
+                const name = qnameLocal(node.name);
+                const schemaNode = resolveSchemaNode(rootSchema, context.schema.properties[name]) || (strict ? null : {type: 'array'});
+                if (!schemaNode) {
+                    console.error(contextStack);
+                    throw new Error('Element <' + node.name + '> cannot be matched against object type in schema.');
+                }
+                contextStack.push({name, value: undefined, schema: schemaNode, attributes: node.attributes});
+                break;
+            }
+            case 'array': {
+                const name = qnameLocal(node.name);
+                const items = normalizeArrayItems(context.schema.items);
+                const schemaNode = resolveSchemaNode(rootSchema, items.find((item) => item.title === name)) || (strict ? null : {type: 'array'});
+                if (!schemaNode) {
+                    console.error(contextStack);
+                    throw new Error('Element <' + node.name + '> cannot be matched against array items in schema.');
+                }
+                contextStack.push({name, value: undefined, schema: schemaNode, attributes: node.attributes});
+                break;
+            }
+            default:
+                throw new Error('Unknown type (in schema): ' + context.schema.type + ' in ' + JSON.stringify(context.schema));
+        }
+    });
+
+    const textHandler = (text) => {
+        const context = contextStack[contextStack.length - 1];
+        let result;
+        if (trimText) {
+            text = text.trim();
+        }
+
+        switch (context.schema.type) {
+            case 'string':
+            case 'integer':
+            case 'number':
+            case 'boolean':
+                if (context.schema.type === 'string') {
+                    result = text;
+                } else if (context.schema.type === 'integer') {
+                    result = parseInt(text);
+                } else if (context.schema.type === 'number') {
+                    result = parseFloat(text);
+                } else {
+                    result = text.toLowerCase();
+                }
+                break;
+            case 'object':
+            case 'array':
+                if (text.length >= 1) {
+                    if (strict) {
+                        throw new Error('Did not expect a text element to match ' + context.schema.type + ' (found "' + text + '" while parsing ' + JSON.stringify(context.schema) + ')');
+                    } else {
+                        result = text;
+                    }
+                }
+                break;
+            default:
+                throw new Error('Unknown type (in schema): ' + context.schema.type + ' in ' + JSON.stringify(context.schema));
+        }
+        if (result !== undefined) {
+            if (context.value !== undefined) {
+                if (strict) {
+                    throw new Error('Multiple text chunks/CData chunks not supported, or found text/CData after child nodes');
+                }
+            } else {
+                context.value = result;
+            }
+        }
+    };
+
+    saxStream.on('cdata', textHandler);
+    saxStream.on('text', textHandler);
+
+    saxStream.on('closetag', () => {
+        const context = contextStack.pop();
+        const parent = contextStack[contextStack.length - 1];
+        let result = context.value;
+        if (result === undefined) {
+            result = null;
+        }
+        if (Object.keys(context.attributes).length >= 1) {
+            result = {'$value': result, '$attributes': context.attributes};
+        }
+        if (parent.schema.type === 'array' && (normalizeArrayItems(parent.schema.items).length >= 2 || Object.keys(parent.attributes).length >= 1)) {
+            if (!Array.isArray(parent.value)) {
+                if (parent.value === undefined) {
+                    parent.value = [];
+                } else {
+                    parent.value = [parent.value];
+                }
+            }
+            parent.value.push({[context.name]: result});
+        } else if (parent.schema.type === 'array' || parent.schema.type === 'object') {
+            if (typeof parent.value !== 'object') {
+                if (parent.value === undefined) {
+                    parent.value = {};
+                } else {
+                    parent.value = {'$value': parent.value};
+                }
+            }
+            parent[context.name] = result;
+        }
+        if (depth === pathDepth) {
+            if (pathDepth === objectPath.length) {
+                // Emit object, clear parent value
+                parent.value = undefined;
+                if (!objectStream.push(result)) {
+                    xmlStream.pause();
+                }
+            }
+            pathDepth -= 1;
+        }
+        depth -= 1;
+    });
+
+    saxStream.on('end', () => {
+        objectStream.push(null);
+    });
+
+    xmlStream.on('error', (error) => {
+        objectStream.emit('error', error);
+    });
+
+    saxStream.on('error', (error) => {
+        objectStream.emit('error', error);
+    });
+
+    objectStream._read = () => {
+        xmlStream.resume();
+    };
+
+    xmlStream.pipe(saxStream);
+
+    return objectStream;
+}
+
+export function toJSON(xmlStream, schema, {strict = false, trimText = true} = {}) {
     const saxStream = sax.createStream(true, {xmlns: false});
     const jsonStream = new stream.Readable();
 
@@ -78,7 +249,7 @@ export default function convert(xmlStream, schema, {strict = false, trimText = t
         schema: rootSchema,
         firstItem: true,
         hasText: false,
-        attributes: []
+        attributes: {}
     }];
 
     saxStream.on('opentag', (node) => {
@@ -178,7 +349,7 @@ export default function convert(xmlStream, schema, {strict = false, trimText = t
                 }
                 if (Object.keys(context.attributes).length >= 1) {
                     result = '{';
-                    result += getAttributesNode(context, strict);
+                    result += getAttributesNodeJSON(context, strict);
                     result += ',"$value":' + value + '}';
                 } else {
                     result = value;
@@ -229,7 +400,7 @@ export default function convert(xmlStream, schema, {strict = false, trimText = t
                 } else {
                     if (Object.keys(context.attributes).length >= 1) {
                         result = '{';
-                        result += getAttributesNode(context, strict);
+                        result += getAttributesNodeJSON(context, strict);
                         result += '}';
                     } else {
                         result = 'null';
@@ -239,7 +410,7 @@ export default function convert(xmlStream, schema, {strict = false, trimText = t
             case 'object':
                 if (Object.keys(context.attributes).length >= 1) {
                     result = context.firstItem ? '' : ',';
-                    result += getAttributesNode(context, strict) + '}';
+                    result += getAttributesNodeJSON(context, strict) + '}';
                 } else {
                     result = '}';
                 }
@@ -247,7 +418,7 @@ export default function convert(xmlStream, schema, {strict = false, trimText = t
             case 'array':
                 if (Object.keys(context.attributes).length >= 1) {
                     result = context.firstItem ? '' : ',';
-                    result += '{' + getAttributesNode(context, strict) + '}]';
+                    result += '{' + getAttributesNodeJSON(context, strict) + '}]';
                 } else {
                     result = ']';
                 }
