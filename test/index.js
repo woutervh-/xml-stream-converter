9aeb6f4d10f7fbbe642a8cd1f0ec705509e0d43d
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2018-08-23 10:52:50 +0200

    Added test file

---
+++ test/index.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const { toObject } = require('../lib/index');
+
+const schema = {
+    type: 'object',
+    properties: {
+        Document: {
+            type: 'array',
+            items: [
+                {
+                    title: 'metaData',
+                    type: 'object',
+                    properties: {
+                        foo: {
+                            type: 'integer'
+                        },
+                        bar: {
+                            type: 'string'
+                        },
+                        created: {
+                            type: 'string'
+                        }
+                    }
+                },
+                {
+                    title: 'item',
+                    type: 'object',
+                    properties: {
+                        message: {
+                            type: 'object',
+                            properties: {
+                                key: {
+                                    type: 'string'
+                                },
+                                version: {
+                                    type: 'integer'
+                                },
+                                contentType: {
+                                    type: 'string'
+                                }
+                            }
+                        },
+                        location: {
+                            type: 'object',
+                            properties: {
+                                position: {
+                                    type: 'string'
+                                }
+                            }
+                        },
+                        event: {
+                            type: 'object',
+                            properties: {
+                                importantEvent: {
+                                    type: 'string'
+                                },
+                                secondaryEvent: {
+                                    type: 'string'
+                                }
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    }
+};
+
+toObject(fs.createReadStream(__dirname + '/test-ns.xml'), schema, ['ns2:Document', 'ns2:item'])
+    .on('data', (data) => {
+        console.log(data);
+    });
