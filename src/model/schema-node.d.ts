be83f2572b3edeea53482f8c4fa44c9bb7427c7a
Guilherme da Silva Amorim <guilherme.amorim@tomtom.com>
2020-02-06 17:43:09 +0100

    Added missing property in schema-node.d.ts

--- src/model/schema-node.d.ts
+++ src/model/schema-node.d.ts
@@ -4,9 +4,10 @@ interface SchemaNode {
     title?: string;
     attributes?: Dictionary<unknown>;
     properties?: Dictionary<SchemaNode>;
-    type: 'string' | 'integer' | 'number' | 'boolean' | 'object' | 'array';
+    type: 'string' | 'integer' | 'number' | 'boolean' | 'object' | 'array' ;
     '$ref'?: string;
     items?: Array<SchemaNode>;
+    format?: string;
 }
 
 // eslint-disable-next-line no-undef
